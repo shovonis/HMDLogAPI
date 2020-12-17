316d6e68c89b44ced2aaad6cc1398f4b680162ac
Rifatul Islam <shovonis09@gmail.com>
2020-12-16 20:38:48 -0600

    Initial Commit

---
+++ Assets/Scripts/Log/FrameCaptureAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Log
+{
+    public class FrameCaptureAPI
+    {
+        public void SaveImageToFile(String path, int frameCount)
+        {
+            // Both Eye
+            String imageFileName = "Frame-" + frameCount + "-.png";
+            ScreenCapture.CaptureScreenshot(path + "/" + imageFileName,
+                ScreenCapture.StereoScreenCaptureMode.BothEyes);
+        }
+    }
+}
